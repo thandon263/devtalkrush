ad24058a0a079fe375bf2aeac8eb8b9e4fa918aa
thandon263 <ncubethando20@gmail.com>
2017-11-02 22:28:30 -0400

    Initial commit

--- public/_include/js/main.js
+++ public/_include/js/main.js
@@ -158,29 +158,29 @@ BRUSHED.filter = function (){
    FancyBox
 ================================================== */
 
-BRUSHED.fancyBox = function(){
-	if($('.fancybox').length > 0 || $('.fancybox-media').length > 0 || $('.fancybox-various').length > 0){
-
-		$(".fancybox").fancybox({
-				padding : 0,
-				beforeShow: function () {
-					this.title = $(this.element).attr('title');
-					this.title = '<h4>' + this.title + '</h4>' + '<p>' + $(this.element).parent().find('img').attr('alt') + '</p>';
-				},
-				helpers : {
-					title : { type: 'inside' },
-				}
-			});
-
-		$('.fancybox-media').fancybox({
-			openEffect  : 'none',
-			closeEffect : 'none',
-			helpers : {
-				media : {}
-			}
-		});
-	}
-}
+// BRUSHED.fancyBox = function(){
+// 	if($('.fancybox').length > 0 || $('.fancybox-media').length > 0 || $('.fancybox-various').length > 0){
+//
+// 		$(".fancybox").fancybox({
+// 				padding : 0,
+// 				beforeShow: function () {
+// 					this.title = $(this.element).attr('title');
+// 					this.title = '<h4>' + this.title + '</h4>' + '<p>' + $(this.element).parent().find('img').attr('alt') + '</p>';
+// 				},
+// 				helpers : {
+// 					title : { type: 'inside' },
+// 				}
+// 			});
+//
+// 		$('.fancybox-media').fancybox({
+// 			openEffect  : 'none',
+// 			closeEffect : 'none',
+// 			helpers : {
+// 				media : {}
+// 			}
+// 		});
+// 	}
+// }
 
 
 /* ==================================================
