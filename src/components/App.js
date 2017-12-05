65eb250dfc5e28455a51064e602d11335bcb2bc0
thandon263 <ncubethando20@gmail.com>
2017-12-05 03:41:52 -0500

    Working on the portfolio page

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Message from "./Message";
+import Header from "./Header";
+
+class App extends React.Component {
+  render() {
+    return <Header />;
+  }
+}
+
+export default App;
---
+++ src/components/Message.js
@@ -0,0 +1,54 @@
+import React from "react";
+import RaisedButton from "material-ui/RaisedButton";
+import Popover, { PopoverAnimationVertical } from "material-ui/Popover";
+import Menu from "material-ui/Menu";
+import MenuItem from "material-ui/MenuItem";
+
+export default class Message extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      open: false
+    };
+  }
+
+  handleClick(event) {
+    // This prevents ghost click.
+    event.preventDefault();
+
+    this.setState({
+      open: true,
+      anchorEl: event.currentTarget
+    });
+  }
+
+  handleRequestClose() {
+    this.setState({
+      open: false
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <RaisedButton onClick={this.handleClick} label="Click me" />
+        <Popover
+          open={this.state.open}
+          anchorEl={this.state.anchorEl}
+          anchorOrigin={{ horizontal: "left", vertical: "bottom" }}
+          targetOrigin={{ horizontal: "left", vertical: "top" }}
+          onRequestClose={this.handleRequestClose}
+          animation={PopoverAnimationVertical}
+        >
+          <Menu>
+            <MenuItem primaryText="Refresh" />
+            <MenuItem primaryText="Help &amp; feedback" />
+            <MenuItem primaryText="Settings" />
+            <MenuItem primaryText="Sign out" />
+          </Menu>
+        </Popover>
+      </div>
+    );
+  }
+}
